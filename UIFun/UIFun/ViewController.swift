bcae9433d482f045cc84f84cad242647ad5b36bc
jellybeanjohnny <mamerige@icloud.com>
2016-07-08 10:08:12 -0400

    Completed lab

--- UIFun/UIFun/ViewController.swift
+++ UIFun/UIFun/ViewController.swift
@@ -10,18 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
     // TODO: Set up IB outlets
+	@IBOutlet weak var paintBucketImageView: UIImageView!
+	@IBOutlet weak var topSegmentedControl: UISegmentedControl!
+	@IBOutlet weak var bottomSegmentedControl: UISegmentedControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // TODO: Set the initial paint color to "red"
+			paintBucketImageView.paintColor = UIColor.redColor()
     }
 
     func mixColors(withFirst first: String, second: String) -> String {
         // TODO: Mix colors and return a string indicating the name of the mixed color
         // (e.g., "red", "purple", "blue")
+			
+			let colorString: String
+			
+			switch (first, second) {
+			case ("Red", "Red"): colorString = "Red"
+			case ("Red", "Yellow"), ("Yellow", "Red"): colorString = "Orange"
+			case ("Red","Blue"), ("Blue", "Red"): colorString = "Purple"
+			case ("Yellow", "Yellow"): colorString = "Yellow"
+			case ("Yellow", "Blue"), ("Blue", "Yellow"): colorString = "Green"
+			case ("Blue", "Blue"): colorString = "Blue"
+			default: colorString = "Red"
+			}
+			
+			return colorString.lowercaseString
     }
 
     @IBAction func colorSelected(sender: UISegmentedControl) {
         // TODO: Mix each selected color and set the paint color to the mixed color
+			let firstColorName = topSegmentedControl.color.name
+			let secondColorName = bottomSegmentedControl.color.name
+			let mixedColor = mixColors(withFirst: firstColorName, second: secondColorName)
+			paintBucketImageView.paintColorName = mixedColor
     }
 }
